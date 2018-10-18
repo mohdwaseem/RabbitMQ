b12220906c1e34f0fe17e91dc5d6cfacb1bdccc8
Mohd Waseem <mohdwaseem488@gmail.com>
2018-10-18 17:15:37 +0300

    Added

---
+++ WebApplicationRabbitMQ/extra/MessageLogger.cs
@@ -0,0 +1,49 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+
+
+namespace Receive
+{
+    public static class MessageLogger
+    {
+
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        /// <summary>
+        /// Method to Save Messages to database
+        /// </summary>
+        /// <param name="correlationId"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static int AddLogs(Guid correlationId, string message)
+        {
+            using (var connection = new SqlConnection("Data Source=W12928\\MSSQLSERVER1;Initial Catalog=RabbitMQLoggingDB;Integrated Security=True"))
+            {
+                connection.Open();
+                var cmd = new SqlCommand("usp_tbl_MessageLogs_SaveLogs", connection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                cmd.Parameters.AddWithValue("@CorrelationId", correlationId);
+                cmd.Parameters.AddWithValue("@MessageData", message);
+                return cmd.ExecuteNonQuery();
+
+            }
+        }
+        public static void AddMessageLogs()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            // thread properties...
+            //log4net.LogicalThreadContext.Properties["CustomColumn"] = "Custom value";
+            // ...or global properties
+            log4net.GlobalContext.Properties["CustomColumn"] = "Custom value";
+            log.Info("Message");
+        }
+    }
+}
+ 
---
+++ WebApplicationRabbitMQ/extra/Receiver.cs
@@ -0,0 +1,58 @@
+﻿using log4net;
+using log4net.Config;
+using RabbitMQ.Client;
+using Receive.Services;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+ 
+
+namespace Receive
+{
+    class Receiver
+    {
+        public static void Main()
+        {
+            
+
+            
+            Console.WriteLine("Please enter Queue Name");
+            //var queuename = Console.ReadLine();
+            var queuename = "efada_certificates";//
+            Console.WriteLine("---------------------Now Reading Messages-------------------");
+
+            try
+            {
+                var factory = new ConnectionFactory { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        var queueDeclareResponse = channel.QueueDeclare(queue: queuename, durable: true, exclusive: false, autoDelete: false, arguments: null);
+                        var consumer = new QueueingBasicConsumer(channel);
+                        channel.BasicConsume(queue: queuename, autoAck: false, consumer: consumer);
+                        for (int i = 0; i < queueDeclareResponse.MessageCount; i++)
+                        {
+                            var ea = consumer.Queue.Dequeue();
+                            var body = ea.Body;
+                            var message = Encoding.UTF8.GetString(body);
+                            Console.WriteLine(" [x] Received {0}", message);
+                            //Saving to database
+                            MessageLogger.AddLogs(Guid.NewGuid(), message);
+                            //Set Acknowledgement to true
+                            channel.BasicAck(ea.DeliveryTag, true);
+                        }
+                        Console.WriteLine("Finished processing {0} messages.", queueDeclareResponse.MessageCount);
+                        Console.ReadLine();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
